b1dfcb741f8712ce0d557955c9affa873f593128
Tuyen Le <tuyen.le106@hcmut.edu.vn>
2026-01-07 11:20:25 +0700

    [chore] Create env file

--- src/utils/constants/api/config.ts
+++ src/utils/constants/api/config.ts
@@ -1,6 +1,8 @@
+import { API_BASE_URL, API_TIMEOUT, API_RETRY_ATTEMPTS } from '@env';
+
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: __DEV__ ? 'http://136.110.55.191:8000' : 'http://136.110.55.191:8000',
-  TIMEOUT: 10000,
-  RETRY_ATTEMPTS: 3,
+  BASE_URL: API_BASE_URL || 'http://localhost:8000',
+  TIMEOUT: Number(API_TIMEOUT) || 10000,
+  RETRY_ATTEMPTS: Number(API_RETRY_ATTEMPTS) || 3,
 };
